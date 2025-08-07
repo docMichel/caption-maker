41a92b8cb2227558d041421ad878a3c1e780154a
NcMichel <mjorda@mls.nc>
2025-08-07 14:24:27 +1100

    importers

---
+++ src/data_import/importers/__init__.py
@@ -0,0 +1,12 @@
+# Classes vides pour l'instant
+class GeoNamesImporter:
+    def __init__(self, db_config):
+        pass
+
+class UNESCOImporter:
+    def __init__(self, db_config):
+        pass
+
+class OSMImporter:
+    def __init__(self, db_config):
+        pass
